4bd09caa571d3ed570cf56eebeaec34c04e80627
Fyodor <fyodor.work@gmail.com>
2018-04-17 10:55:25 +0300

    added plus-minus for menus

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,1 +1,7 @@
-//= _nav/nav.js
+
+$(document).ready(function () {
+    //console.log("header.js");
+    $('body').on('click', '.plusIcon', function () {
+        $('.plus').toggleClass('plus-to-minus')
+    })
+});
--- source/blocks/index/index.js
+++ source/blocks/index/index.js
@@ -1,3 +1,5 @@
+//= ../header/header.js
+
 $(document).ready(function () {
     //console.log("index.js");
 });
